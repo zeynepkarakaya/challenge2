e25eb4b68a02563aeb3090461ff8eaa4a902a962
zeynepkarakaya <46029791+zeynepkarakaya@users.noreply.github.com>
2021-06-01 00:57:57 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ function updateTime(){
 
   var marsDay = new Date();
   document.getElementById('date').innerHTML = marsDay.getDate() + ' / ' + (marsDay.getMonth()+1) + ' / ' + (marsDay.getFullYear()+11);
-  document.getElementById('time').innerHTML = addLeadingZero(marsDay.getHours()) + ' : ' + addLeadingZero(marsDay.getMinutes()) + ' : ' + addLeadingZero(marsDay.getSeconds());
+  document.getElementById('time').innerHTML = changebg(addLeadingZero(marsDay.getHours())) + ' : ' + addLeadingZero(marsDay.getMinutes()) + ' : ' + addLeadingZero(marsDay.getSeconds());
   function addLeadingZero(number){
     if (number < 10) {
       number = '0'+ number;
@@ -13,11 +13,17 @@ function updateTime(){
 
 setInterval (updateTime , 1000);
 
-var bg = ['bgchange' , 'bgchange1' , 'bgchange2' , 'bgchange3' , 'bgchange4' , 'bgchange5' , 'bgchange6'];
+function changebg(number){
+    if (number < 5 || number > 20) {
+      document.getElementById('message').innerHTML = 'Time to sleep!';
+    } else if (5 < number < 20) {
+      document.getElementById('message').innerHTML = 'Have a nice day!';
+    }
+    return number;
+  }
+
+var bg = ['bgchange' , 'bgchange1' , 'bgchange2' , 'bgchange3' , 'bgchange4' , 'bgchange5' , 'bgchange6' , 'bgchange7' , 'bgchange8' , 'bgchange9' , 'bgchange10'];
 document.getElementById('body').onclick = function(){
   document.getElementById('body').className = bg[Math.floor(Math.random()*bg.length)];
 };
-
-
-
 
